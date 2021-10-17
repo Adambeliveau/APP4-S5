5b6642fc2ffbedde1a2da5f24d87b9f549cc26d1
Adam <bela1003@usherbrooke.ca>
2021-10-17 11:45:07 -0400

    added filter graphic

--- filtering.py
+++ filtering.py
@@ -1,6 +1,12 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 
+wp = 500
+ws = 750
+gpass = 0.2
+gstop = 60
+fs = 1600
 
 def transfer_fct(method: str):
     if method == 'bilineaire':
@@ -23,29 +29,40 @@ def transfer_fct(method: str):
 
 def filter_img(img, method: str):
     b, a = transfer_fct(method)
+    w, h = signal.freqz(b, a)
+    plt.figure()
+    plt.plot(w, 20 * np.log10(np.abs(h)))
+    plt.title(f'{method} filter')
+    plt.ylabel('amplitude (dB)')
+    plt.xlabel('w (éch/rad)')
+    plt.axvline((2 * np.pi * ws)/fs, color='r')
+    plt.axvline((2 * np.pi * wp)/fs, color='g')
+    plt.axhline(-gpass, color='g')
+    plt.axhline(-gstop, color='r')
+    plt.savefig(f'Ressources/{method}_filter.png')
 
     return signal.lfilter(b, a, img)
 
 
 def butter():
-    n, W_n = signal.buttord(500, 750, 0.2, 60, fs=1600)
+    n, W_n = signal.buttord(wp, ws, gpass, gstop, fs=fs)
     print(n)
-    return signal.butter(n, W_n, fs=1600)
+    return signal.butter(n, W_n, fs=fs)
 
 
 def cheby1():
-    n, W_n = signal.cheb1ord(500, 750, 0.2, 60, fs=1600)
+    n, W_n = signal.cheb1ord(wp, ws, gpass, gstop, fs=fs)
     print(n)
-    return signal.cheby1(n, 0.2, W_n, fs=1600)
+    return signal.cheby1(n, gpass, W_n, fs=1600)
 
 
 def cheby2():
-    n, W_n = signal.cheb2ord(500, 750, 0.2, 60, fs=1600)
+    n, W_n = signal.cheb2ord(wp, ws, gpass, gstop, fs=fs)
     print(n)
-    return signal.cheby2(n, 0.2, W_n, fs=1600)
+    return signal.cheby2(n, gstop, W_n, fs=fs)
 
 
 def ellip():
-    n, W_n = signal.ellipord(500, 750, 0.2, 60, fs=1600)
+    n, W_n = signal.ellipord(wp, ws, gpass, gstop, fs=fs)
     print(n)
-    return signal.ellip(n, 0.2, 60, W_n, fs=1600)
+    return signal.ellip(n, gpass, gstop, W_n, fs=fs)
